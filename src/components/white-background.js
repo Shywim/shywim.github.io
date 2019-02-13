264bccbaabefa9d75e3410175f56d4ef5b282958
Matthieu Harlé <bonjour@matthieuharle.com>
2019-02-13 23:13:06 +0100

    New version

--- src/components/white-background.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  background: white;
-  height: ${props => props.height}px;
-  width: 100%;
-  transform: skewY(-8deg);
-  transform-origin: 0;
-`
