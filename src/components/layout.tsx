825f4b7eac6e52cfacc20749e19920bfef6a6239
Matthieu Harlé <bonjour@matthieuharle.com>
2021-09-28 18:53:33 +0200

    add back plausible script

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import "./layout.css"
 
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return <div>
+    <Helmet>
+      {process.env.NODE_ENV === 'production' && <script async defer data-domain="matthieuharle.com" src="https://p.matthieuharle.com/js/script.js"></script>}
+    </Helmet>
+    {children}
+  </div>
 }
